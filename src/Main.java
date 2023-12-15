f34eedf45899cb718bd257cfea8162a2b95b5b8f
MarvelVCO <marvelo6@nycstudents.net>
2023-12-15 10:45:51 -0500

    added a new file and line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is the first commit that added code. Now my partner will commit a new line:");
         System.out.println("Marvel's a dog...");
+        System.out.println("Whats up dog");
     }
 }
---
+++ src/NewFile.java
@@ -0,0 +1,7 @@
+public class NewFile {
+    public static void printALotOfXs() {
+        for(int i = 0; i < 1000000; i++) {
+            System.out.println("x");
+        }
+    }
+}
