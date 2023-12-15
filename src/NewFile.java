3a6273833b471cab70a3209e20213107ba5a4ae5
MarvelVCO <marvelo6@nycstudents.net>
2023-12-15 10:50:42 -0500

    added printALotOfYs to NewFile

--- src/NewFile.java
+++ src/NewFile.java
@@ -4,4 +4,10 @@ public class NewFile {
             System.out.println("x");
         }
     }
+
+    public static void printALotOfYs() {
+        for(int i = 0; i < 1000000; i++) {
+            System.out.println("y");
+        }
+    }
 }
