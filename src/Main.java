4e00f6950a87a2e8a1fdf39b52da8cbeb3fe8167
FirePheonix123 <muhammadt47@nycstudents.net>
2023-12-15 10:55:11 -0500

    Part tres

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("This is the first commit that added code. Now my partner will commit a new line:");
         System.out.println("Marvel's a dog...");
         System.out.println("Whats up dog");
+        System.out.println("How does Marvel know so much about git?");
+        System.out.println("It's my job, *Insert Marvel Noises*");
     }
 }
