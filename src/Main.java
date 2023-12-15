01cd1348f2a5085815d2fde6510655662b50f636
MarvelVCO <marvelo6@nycstudents.net>
2023-12-15 10:36:26 -0500

    first line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("This is the first commit that added code. Now my partner will commit a new line:");
     }
 }
